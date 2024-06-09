d4c4ca973ac0f1e4fe539bcb7bd72172d7e964e3
Elisha Ukpong <ishukpong418@gmail.com>
2024-06-09 21:46:37 +0100

    added setup script to setup env file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
 #!/bin/zsh
+
+env_file_path="./.env"
+env_example_file_path="./.env.example"
+
+if [ -f "$env_file_path" ]; then
+    echo "Environment file already exists."
+else
+
+  if [ -f "$env_example_file_path" ]; then
+    cp .env.example .env
+    echo "Environment file created."
+  else
+    echo "Example environment file is missing."
+  fi
+
+fi
